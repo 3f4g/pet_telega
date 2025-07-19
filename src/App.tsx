6d2189458022869bb318691a2e4606ca8aa38da3
ars <kigetan747@provko.com>
2025-07-19 19:27:59 +0300

    add firebase to project

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,33 @@ import "./index.css";
 import logo from "./logo.svg";
 import reactLogo from "./react.svg";
 
+import { initializeApp } from "firebase/app";
+
 export function App() {
+
+// Import the functions you need from the SDKs you need
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAa1J12-YAXPmaKaDQanfiCW3fbIJjp_SI",
+  authDomain: "pet-messenger.firebaseapp.com",
+  projectId: "pet-messenger",
+  storageBucket: "pet-messenger.firebasestorage.app",
+  messagingSenderId: "725978302343",
+  appId: "1:725978302343:web:a95808aab1c0587d247522"
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+
   return (
     <div className="container mx-auto p-8 text-center relative z-10">
       <div className="flex justify-center items-center gap-8 mb-8">
